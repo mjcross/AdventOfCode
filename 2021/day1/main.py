41bda8f931b325e49ae2510d45da814ea4200b34
martin <admin@crossleys.biz>
2022-12-06 13:34:31 +0000

    2021 day1

---
+++ 2021/day1/main.py
@@ -0,0 +1,43 @@
+tests = [
+    ((199, 200, 208, 210, 200, 207, 240, 269, 260, 263), (7, 5))
+]
+
+
+def delta(seq):
+    previtem = seq[0]
+    for thisitem in seq[1:]:
+        yield thisitem - previtem
+        previtem = thisitem
+
+
+def windows(seq, size):
+    i = 0
+    while i + size <= len(seq):
+        yield(seq[i: i + size])
+        i += 1
+
+
+def numincreases(depths):
+    return sum(delta > 0 for delta in delta(depths))
+
+
+def part1(seq):
+    return numincreases(seq)
+
+
+def part2(seq):
+    return numincreases([sum(window) for window in windows(seq, 3)])
+
+
+def main():
+    for testdata, answer in tests:
+        assert part1(testdata) == answer[0]
+        assert part2(testdata) == answer[1]
+
+    with open('input.txt') as infile:
+        depthdata = [int(depth) for depth in infile.readlines()]
+        print(part1(depthdata))
+        print(part2(depthdata))
+
+if __name__ == '__main__':
+    main()
--- 2022/sandbox/hacks.py
+++ 2022/sandbox/hacks.py
@@ -1,6 +1,19 @@
-from collections import OrderedDict
+seq = (199, 200, 208, 210, 200, 207, 240, 269, 260, 263)
 
-od = OrderedDict({2: 'two', 1: 'one', 3: 'three'})
-d = {2: 'two', 1: 'one', 3: 'three'}
-print(od.keys())
-print(d.keys())
+class mylist(list):
+    
+    def delta(self):
+        previtem = self[0]
+        for thisitem in self[1:]:
+            yield thisitem - previtem
+            previtem = thisitem
+
+
+    def windowed(self, size):
+        i = 0
+        while i + size <= len(seq):
+            yield(self[i: i + size])
+            i += 1
+
+li = mylist(range(10))
+print(li, list(li.delta()), list(li.windowed(4)))
