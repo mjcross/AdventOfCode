e0536623420001a558f9778a01af12c889fe3527
martin <admin@crossleys.biz>
2022-12-19 13:50:39 +0000

    add 2022 day16 part 1

--- 2022/day16/main.py
+++ 2022/day16/main.py
@@ -1,26 +1,30 @@
 """
 Observations about the puzzle
 =============================
-1) The optimal route often involves passing through a room without stopping to
-turn on a slow valve
-2) Before starting to solve the graph, cull out all zero-rated rooms with only
-one way in and one way out ('transit nodes')... except the start node!
-3) Do NOT try to find the optimal route by trying every possible 30min path 
-through the graph. Instead, pick the next sensible destination (a room with a 
-turn-on-able valve); travel by the lowest cost route, without stopping, and
-turn on the valve. You can then exhaust over all possible sequences of valves.
-4) The cheapest route between any pair of nodes without stopping doesn't
-change, so it can be pre-computed.
-5) Heuristics like not going down blind alleys once, only traversing tunnels
-once in each direction etc appear to be negated by the puzzle mechanic. I
-think you're better off doing least-cost navigation between chosen nodes.
+1) The optimum route involves navigating by the shortest path between nodes
+where you want to DO something (in this case, turn on a valve). So ignore the
+zero-flowrate valves and just navigate between the useful ones.
+2) The cost of the 'cheapest' route between nodes doesn't change, so you can
+precompute it.
+3) Unfortunately the puzzle has 15 non-zero nodes so there are 10^12 different
+ways to visit them all. This means ANY approach that involves checking every 
+possible sequence (even really fast) is doomed. 
+4) Some kind of intelligent approach (or at least a heuristic) is therefore
+needed. 
+    - One suggestion is to limit the amount of 'look-ahead' to a given depth, 
+      and choose the best option based on that.
+    - Another idea is to give every possible next destination a score based
+      on benefit (which in this case depends on time) minus cost
+    - An observation is that the 'high level' structure of the graph MATTERS.
+      Cul-de-sacs demand a different strategy to loops.
 """
 
 from utils.route import Node, leasthops, hoptable, showhoptable
 from itertools import permutations
 from copy import copy
+import timeit
 
-debug = 3
+debug = 1
 
 class Valve(Node):
     def __init__(self, name, flowrate, edges):
@@ -54,7 +58,55 @@ def parse(stream):
     return valvedict
 
 
+def topcut(current, flowtotal, cutsize, timeleft, leasthops, valvesleft, path):
+    """Recursively explore the top 'cutsize' moves and return the best score."""
+    path.append(current.name)
+ 
+    if current.flowrate > 0 and timeleft > 1:
+        # open valve at current location
+        timeleft -= 1
+        flowtotal += timeleft * current.flowrate
+        if debug >= 2:
+            print(f'opened {current.name} rate {current.flowrate:2d} with {timeleft:2d} mins left = {current.flowrate * timeleft:3d} (total {flowtotal})')
+
+    if timeleft <= 1 or not valvesleft:
+        # out of time or all valves open
+        if debug >= 2:
+            print(f'{" ".join(path)} = {flowtotal}\t({timeleft} mins left, {len(valvesleft)} valves left)\n')
+        return flowtotal
+    else:
+        # identify best options for next step
+        candidates = [(valve, valve.flowrate * (timeleft - leasthops[current, valve] - 1)) for valve in valvesleft]
+        candidates.sort(key=lambda x: x[1], reverse=True)
+        candidates = candidates[:cutsize]
+
+        bestflowtotal = flowtotal
+        for nextvalve, flow in candidates:
+            newvalvesleft = copy(valvesleft)
+            newvalvesleft.remove(nextvalve)
+            newflowtotal = topcut(
+                current=nextvalve, 
+                flowtotal=flowtotal, 
+                cutsize=cutsize, 
+                timeleft=timeleft - leasthops[current, nextvalve],
+                leasthops=leasthops, 
+                valvesleft=newvalvesleft,
+                path = copy(path))
+
+            bestflowtotal = max(newflowtotal, bestflowtotal)
+
+        return bestflowtotal
+
+
 def part1(stream):
+    valvedict = parse(stream)
+    start = valvedict['AA']
+    valves = [valve for valve in valvedict.values() if valve.flowrate]
+    leasthops = hoptable([start] + copy(valves))
+    return topcut(current=start, flowtotal=0, cutsize=8, timeleft=30, leasthops=leasthops, valvesleft=valves, path=[])
+
+
+def part1exhaust(stream):
     valvedict = parse(stream)
     if debug >= 3:
         for name, valve in valvedict.items():
@@ -116,7 +168,10 @@ def main():
     checkexamples()
 
     with open('input.txt') as stream:
+        start = timeit.default_timer()
         print('part1', part1(stream))
+        stop = timeit.default_timer()
+        print(f'took {stop-start:.2f} sec')
 
     #with open('input.txt') as stream:
     #    print('part2', part2(stream))
