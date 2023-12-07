fd4ea8b5bf27b00300964bf3f9d542456781813e
mjcross <admin@crossleys.biz>
2023-12-07 21:12:45 +0000

    add Day 5 solutions

--- 2023/day 5/main.py
+++ 2023/day 5/main.py
@@ -4,21 +4,22 @@ from map import Span
 
 def part1(stream):
     seeds, maps = parse(stream)
-    for mapname in ['seed-to-soil',
+    for mapName in ['seed-to-soil',
                     'soil-to-fertilizer',
                     'fertilizer-to-water',
                     'water-to-light',
                     'light-to-temperature',
                     'temperature-to-humidity',
                     'humidity-to-location']:
-        seeds = map(maps[mapname].map, seeds)
+        seeds = map(maps[mapName].map, seeds)
     return min(seeds)
 
 
 def part2(stream):
+    # for part(ii) we must interpret pairs of "seed numbers" as (start, len)
     seeds, maps = parse(stream)
 
-    # convert seeds to spans
+    # convert seed numbers into 'Span(first, last)'
     spans = []
     while seeds:
         spanLen = seeds.pop()
@@ -27,11 +28,22 @@ def part2(stream):
         spanStop = spanStart + spanLen - 1
         spans.append(Span(spanStart, spanStop))
 
-    newSpans = maps['seed-to-soil'].mapSpans(spans)
-    print(spans)
-    print(newSpans)
-    pass
-
+    # apply the maps to the spans
+    for mapName in ['seed-to-soil',
+                    'soil-to-fertilizer',
+                    'fertilizer-to-water',
+                    'water-to-light',
+                    'light-to-temperature',
+                    'temperature-to-humidity',
+                    'humidity-to-location']:
+        spans = maps[mapName].mapSpans(spans)
+    
+    # the puzzle asks us to find the lowest location number
+    lowestLocation = None
+    for span in spans:
+        if lowestLocation is None or span.start < lowestLocation:
+            lowestLocation = span.start
+    return lowestLocation
 
 
 def checkexamples():
@@ -43,7 +55,7 @@ def checkexamples():
     with open('example.txt') as stream:
         result = part2(stream)
         print(f'example2: {result}')
-    #    assert result == 'xxxxx', result
+        assert result == 46, result
 
 
 def main():
@@ -53,9 +65,9 @@ def main():
         result = part1(stream)
         print(f'part1 {result}')
 
-    #with open('input.txt') as stream:
-    #    result = part2(stream)
-    #    print(f'part2 {result}')
+    with open('input.txt') as stream:
+        result = part2(stream)
+        print(f'part2 {result}')
 
 
 if __name__ == '__main__':
--- 2023/day 5/map.py
+++ 2023/day 5/map.py
@@ -5,12 +5,18 @@ class Span:
     start: int
     stop: int
 
+    def __repr__(self):
+        return f'{self.start}-{self.stop}'
+
 
 @dataclass
 class Mapping:
     destStart: int
     srcStart: int
     rangeLen: int
+
+    def __repr__(self):
+        return f'{self.srcStart}-{self.srcStop} -> {self.destStart}-{self.destStop}'
 
     @property
     def destRange(self):
@@ -36,23 +42,36 @@ class Mapping:
             return source
 
     def mapSpan(self, span):
-        newSpans = []
+        leader = None
         if span.start < self.srcStart:
             # unmapped leading part
             leaderStart = span.start
             leaderStop = min(span.stop, self.srcStart - 1)
-            newSpans.append(Span(leaderStart, leaderStop))    
+            leader = Span(leaderStart, leaderStop)
+
+        trailer = None
         if span.stop > self.srcStop:
             # unmapped trailing part
             trailerStop = span.stop
             trailerStart = max(span.start, self.srcStop + 1)
-            newSpans.append(Span(trailerStart, trailerStop))
+            trailer = Span(trailerStart, trailerStop)
+
+        overlap = None
         overlapStart = max(self.srcStart, span.start)
         overlapStop = min(self.srcStop, span.stop)
         if overlapStart <= span.stop and overlapStop >= span.start:
             # mapped overlapping part
-            newSpans.append(Span(self.map(overlapStart), self.map(overlapStop)))
-        return newSpans
+            overlap = Span(self.map(overlapStart), self.map(overlapStop))
+
+        mapped = None
+        unmapped = []
+        if leader:
+            unmapped.append(leader)
+        if trailer:
+            unmapped.append(trailer)
+        if overlap:
+            mapped = overlap
+        return (unmapped, mapped)
 
 @dataclass
 class Map:
@@ -66,12 +85,17 @@ class Map:
         return source
         
     def mapSpans(self, spans):
-        newSpans = []
-        for span in spans:
-            for mapping in self.mappings:
-                newSpans += mapping.mapSpan(span)
-        return newSpans
-    
+        mappedSpans = []
+        for mapping in self.mappings:
+            unmappedSpans = []   # unmapped spans for the next mapping
+            for span in spans:
+                unmapped, mapped = mapping.mapSpan(span)
+                if unmapped:
+                    unmappedSpans += unmapped
+                if mapped:
+                    mappedSpans.append(mapped)
+            spans = unmappedSpans.copy()
+        return mappedSpans + unmappedSpans
 
 
 def main():
@@ -79,21 +103,21 @@ def main():
     mapping = Mapping(srcStart=10, destStart=100, rangeLen=10)  # {10...19} -> {100...119}
 
     # check all the different overlaps and edge cases
-    assert mapping.mapSpan(Span(5, 5)) == [Span(5, 5)]
-    assert mapping.mapSpan(Span(5, 9)) == [Span(5, 9)]
-    assert mapping.mapSpan(Span(5, 10)) == [Span(5, 9), Span(100, 100)]
-    assert mapping.mapSpan(Span(5, 15)) == [Span(5, 9), Span(100, 105)]
-    assert mapping.mapSpan(Span(5, 19)) == [Span(5, 9), Span(100, 109)]
-    assert mapping.mapSpan(Span(5, 19)) == [Span(5, 9), Span(100, 109)]
-    assert mapping.mapSpan(Span(10, 19)) == [Span(100, 109)]
-    assert mapping.mapSpan(Span(12, 17)) == [Span(102, 107)]
-    assert mapping.mapSpan(Span(15, 25)) == [Span(20, 25), Span(105, 109)]
-    assert mapping.mapSpan(Span(19, 25)) == [Span(20, 25), Span(109, 109)]
-    assert mapping.mapSpan(Span(20, 25)) == [Span(20, 25)]
-    assert mapping.mapSpan(Span(25, 30)) == [Span(25, 30)]
-    assert mapping.mapSpan(Span(10, 19)) == [Span(100, 109)]
-    assert mapping.mapSpan(Span(9, 20)) == [Span(9, 9), Span(20, 20), Span(100, 109)]
-    assert mapping.mapSpan(Span(5, 25)) == [Span(5, 9), Span(20, 25), Span(100, 109)]
+    # the fields are ([leader, trailer], overlap)
+    assert mapping.mapSpan(Span(5, 5)) == ([Span(5, 5)], None)
+    assert mapping.mapSpan(Span(5, 9)) == ([Span(5, 9)], None)
+    assert mapping.mapSpan(Span(5, 10)) == ([Span(5, 9)], Span(100, 100))
+    assert mapping.mapSpan(Span(5, 15)) == ([Span(5, 9)], Span(100, 105))
+    assert mapping.mapSpan(Span(5, 19)) == ([Span(5, 9)], Span(100, 109))
+    assert mapping.mapSpan(Span(10, 19)) == ([], Span(100, 109))
+    assert mapping.mapSpan(Span(12, 17)) == ([],Span(102, 107))
+    assert mapping.mapSpan(Span(15, 25)) == ([Span(20, 25)], Span(105, 109))
+    assert mapping.mapSpan(Span(19, 25)) == ([Span(20, 25)], Span(109, 109))
+    assert mapping.mapSpan(Span(20, 25)) == ([Span(20, 25)], None)
+    assert mapping.mapSpan(Span(25, 30)) == ([Span(25, 30)], None)
+    assert mapping.mapSpan(Span(10, 19)) == ([], Span(100, 109))
+    assert mapping.mapSpan(Span(9, 20)) == ([Span(9, 9), Span(20, 20)], Span(100, 109))
+    assert mapping.mapSpan(Span(5, 25)) == ([Span(5, 9), Span(20, 25)], Span(100, 109))
 
 if __name__ == '__main__':
     main()
